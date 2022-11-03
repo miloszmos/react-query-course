0e4f1c1d9c804d42cab3af3ac4f4b543ea7092ad
miloszmos <milosz.moska@gmail.com>
2022-11-03 23:31:42 +0100

    added assignee to issueitem

--- src/components/IssueItem.jsx
+++ src/components/IssueItem.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { GoIssueOpened, GoIssueClosed, GoComment } from 'react-icons/go';
 import { relativeDate } from '../helpers/relativeDate';
+import { useUserData } from '../helpers/useUserData';
 
 export function IssueItem({
   title,
@@ -13,6 +14,9 @@ export function IssueItem({
   labels,
   status,
 }) {
+  const assigneeUser = useUserData(assignee);
+  const createdByUser = useUserData(createdBy);
+
   return (
     <li>
       <div>
@@ -32,10 +36,21 @@ export function IssueItem({
           ))}
         </span>
         <small>
-          #{number} opened {relativeDate(createdDate)} by {createdBy}
+          #{number} opened {relativeDate(createdDate)}{' '}
+          {createdByUser.isSuccess && `by ${createdByUser.data.name}`}
         </small>
       </div>
-      {assignee ? <div>{assignee}</div> : null}
+      {assignee ? (
+        <img
+          className="assigned-to"
+          alt={`Assigned to ${
+            assigneeUser.isSuccess ? assigneeUser.data.name : 'user'
+          }`}
+          src={
+            assigneeUser.isSuccess ? assigneeUser.data.profilePictureUrl : ''
+          }
+        />
+      ) : null}
       <span className="comment-count">
         {commentCount > 0 ? (
           <>
